8218b5a9312fb78f384acd5f0445a8c7d186bcbe
Hongxu <hongxuchen1989@gmail.com>
2018-07-03 22:33:36 +0800

    more mjs

---
+++ mjs-8d847f2/crashes/assert_mjs.c:10881_1.js
@@ -0,0 +1,4 @@
+let o = {
+  q: 1,
+  foo: ffi('int (int),,,   ÿÿÿ€                                                                        3                                           (int)'),
+};
